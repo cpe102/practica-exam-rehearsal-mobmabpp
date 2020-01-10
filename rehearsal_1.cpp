36535b4d1d3ed7962cbb8b0d0d6f45ddb7fd46cf
Mobmab_Lee <59276838+mobmabpp@users.noreply.github.com>
2020-01-10 16:17:14 +0700

    Update rehearsal_1.cpp

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+   double gpa=0;
+   string name;
+
+   cout<<"What is your name?:";
+   cin>>name;
+   cout<<"Whait is your GPA?:";
+   cin>>gpa;
+
+   if(gpa>=3.50)
+   {
+       cout<<name<<"  InW Jrim Jrim!!!"<<endl;
+   }
+   else
+   {
+       cout<<"Try harder, "<<name<<endl;
+   }   
+    
+    return 0;
+}
 
