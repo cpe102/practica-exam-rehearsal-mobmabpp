e055001925e7cadec55d9e0394495b9d65947397
Mobmab_Lee <59276838+mobmabpp@users.noreply.github.com>
2020-01-10 16:59:08 +0700

    Update rehearsal_2.cpp

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,21 @@
+#include<iostrean>
+using namespace std;
+
+int main(){
+
+    double x,sum=0;
+
+    cout<<"Enter x: ";
+    cin>>x;
+
+    if(x=0)
+    {
+        cout<<"sum = 10";
+    }
+    else
+    {
+
+    }
+    return 0;
+}
 
